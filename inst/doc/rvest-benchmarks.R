583ff08710eac92b706797e04066dbadd379dc65
Jordan Bradford <jrdnbradford@gmail.com>
2025-01-25 21:00:02 +0000

    version 0.3.1

--- inst/doc/rvest-benchmarks.R
+++ inst/doc/rvest-benchmarks.R
@@ -21,50 +21,50 @@ test_rvest <- function(content) {
   rvest::read_html(content) |> rvest::html_table()
 }
 
-## ----benchmark-1--------------------------------------------------------------
+## ----benchmark-1, fig.alt = "Violin plot showing that extract_md_tables is faster than rvest"----
 mtcars_split_raw_md_url <- "https://raw.githubusercontent.com/jrdnbradford/readMDTable/refs/heads/main/inst/extdata/mtcars-split.md"
 mtcars_split_html_url <- "https://github.com/jrdnbradford/readMDTable/blob/main/inst/extdata/mtcars-split.md"
 
 resp <- microbenchmark::microbenchmark(
   test_extract_md_tables(mtcars_split_raw_md_url),
   test_rvest(mtcars_split_html_url),
-  times = 50
+  times = 20
 )
 
 ggplot2::autoplot(resp)
 
-## ----benchmark-2--------------------------------------------------------------
+## ----benchmark-2, fig.alt = "Violin plot showing that read_md_table is faster than rvest"----
 mtcars_raw_md_url <- "https://raw.githubusercontent.com/jrdnbradford/readMDTable/refs/heads/main/inst/extdata/mtcars.md"
 mtcars_html_url <- "https://github.com/jrdnbradford/readMDTable/blob/main/inst/extdata/mtcars.md"
 
 resp <- microbenchmark::microbenchmark(
   test_read_md_table(mtcars_raw_md_url),
   test_rvest(mtcars_html_url),
-  times = 50
+  times = 20
 )
 
 ggplot2::autoplot(resp)
 
-## ----benchmark-3--------------------------------------------------------------
+## ----benchmark-3, fig.alt = "Violin plot showing that rvest is faster than extract_md_tables"----
 mtcars_raw_md_file <- readr::read_file(mtcars_split_raw_md_url)
 mtcars_html_file <- readr::read_file(mtcars_split_html_url)
 
 resp <- microbenchmark::microbenchmark(
   test_extract_md_tables(mtcars_raw_md_file),
   test_rvest(mtcars_html_file),
-  times = 50
+  times = 100
 )
 
 ggplot2::autoplot(resp)
 
-## ----benchmark-4--------------------------------------------------------------
+## ----benchmark-4, fig.alt = "Violin plot showing that rvest is slighlty faster than read_md_table"----
 raw_md <- readr::read_file(mtcars_raw_md_url)
 html <- readr::read_file(mtcars_html_url)
 
 resp <- microbenchmark::microbenchmark(
   test_read_md_table(raw_md),
   test_rvest(html),
-  times = 50
+  times = 100
 )
 
 ggplot2::autoplot(resp)
